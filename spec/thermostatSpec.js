138b1d0115e358509db8fb0c8dc5de02db8d88a5
samover <samuel.overloop@gmail.com>
2015-11-11 14:34:49 +0000

    test for increasing temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,7 +6,11 @@ describe('Thermostat', function(){
   });
 
   it('it has a starting temperature of 20', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature).toEqual(thermostat.DEF_TEMP);
   });
 
+  it('can increase the temperature', function() {
+    thermostat.up();
+   expect(thermostat.temperature).toEqual(thermostat.DEF_TEMP + 1);
+  });
 });
---
+++ src/thermostat.js
@@ -0,0 +1,4 @@
+function Thermostat() {
+  this.DEF_TEMP = 20
+  this.temperature = this.DEF_TEMP;
+}
