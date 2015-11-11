2a1f2550c004e07aab72ec344d83d4c16ae5c3bd
samover <samuel.overloop@gmail.com>
2015-11-11 15:24:10 +0000

    test for max temp reached

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,18 +1,22 @@
+ //'use strict';
+
 describe('Thermostat', function(){
   var thermostat;
   var down;
   var min_temp;
   var def_temp;
+  var up;
 
   beforeEach(function(){
     thermostat = new Thermostat();
     down = function() { thermostat.down(); };
     min_temp = thermostat.MIN_TEMP;
     def_temp = thermostat.DEF_TEMP;
+    up = function() { thermostat.up(); };
   });
 
   it('it has a starting temperature of' + def_temp, function(){
-    expect(thermostat.temperature).toEqual(def_temp);
+    expect(thermostat.showTemperature()).toEqual(def_temp);
   });
 
   // As a user
@@ -27,23 +31,33 @@ describe('Thermostat', function(){
 
   it('can increase the temperature', function() {
     thermostat.up();
-   expect(thermostat.temperature).toEqual(def_temp + 1);
+   expect(thermostat.showTemperature()).toEqual(def_temp + 1);
   });
 
   it('can decrease the temperature', function(){
     thermostat.down();
-    expect(thermostat.temperature).toEqual(def_temp - 1);
+    expect(thermostat.showTemperature()).toEqual(def_temp - 1);
   });
 
   it('power saving mode can be turned off', function(){
     thermostat.turnPowerSavingOff();
-    expect(thermostat.powerSaving).toBe(false);
+    expect(thermostat.showPowerSaving()).toBe(false);
   });
 
   it('power saving mode can be turned on', function(){
     thermostat.turnPowerSavingOff();
     thermostat.turnPowerSavingOn();
-    expect(thermostat.powerSaving).toBe(true);
+    expect(thermostat.showPowerSaving()).toBe(true);
   });
 
+  it('throws error when max_temp with powersaving on', function() {
+    for(var i = 0; i < max_temp(); i++) { thermostat.up(); }
+    expect(up).toThrowError('Maximum temperature reached');
+  });
+
+  it('throws error when max_temp with powersaving off', function() {
+    thermostat.turnPowerSavingOff();
+    for(var i = 0; i < max_temp(); i++) { thermostat.up(); }
+    expect(up).toThrowError('Maximum temperature reached');
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,31 @@
 function Thermostat() {
   this.DEF_TEMP = 20;
   this.MIN_TEMP = 10;
-  this.temperature = this.DEF_TEMP;
+  this._temperature = this.DEF_TEMP;
+  this._powerSaving = true;
 }
+
+Thermostat.prototype.showTemperature = function(){
+  return this._temperature;
+};
 
 Thermostat.prototype.up = function(){
-  this.temperature ++;
-}
+  this._temperature ++;
+};
 
 Thermostat.prototype.down = function() {
-  if (this.temperature <= this.MIN_TEMP) { throw new Error('Minimum temperature reached');}
-  this.temperature --;
-}
+  if (this._temperature <= this.MIN_TEMP) { throw new Error('Minimum temperature reached');}
+  this._temperature --;
+};
+
+Thermostat.prototype.turnPowerSavingOff = function() {
+  this._powerSaving = false;
+};
+
+Thermostat.prototype.turnPowerSavingOn = function() {
+  this._powerSaving = true;
+};
+
+Thermostat.prototype.showPowerSaving = function() {
+  return this._powerSaving;
+};
