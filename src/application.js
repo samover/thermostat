e5cd0fb70c8381bb1bd7fb6a8f2ebb02fcb6b0ca
alaanzr <alaanzr@gmail.com>
2015-11-12 15:31:28 +0000

    (WIP) Implementing Open Weather API functionality

--- src/application.js
+++ src/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   thermostat = new Thermostat();
   displayReset();
+  var owData;
+  displayWeather();
 
   $('#up').click(function() {
     thermostat.up();
@@ -31,6 +33,18 @@ $(document).ready(function() {
     $('#showTemperature').text(thermostat.showTemperature());
     $('#display').attr('class', thermostat.display());
     $('span#isPowerSaving').text(thermostat.isPowerSaving() ? 'ON' : 'OFF');
+  }
+
+  function displayWeather() {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0', function(data) {
+      owData = data;
+      $('#location').text(owData.name + ', ' + owData.sys.country);
+      $('#temperature').text((owData.main.temp - 272.15).toFixed(1) + ' degrees Celsius');
+      $('#windSpeed').text(owData.wind.speed + 'm/s');
+      $('#weatherDescription').text(owData.weather.description);
+      $('$icon').text("<img src= " + owData.weather.icon + ">");
+
+    });
   }
 
 });
