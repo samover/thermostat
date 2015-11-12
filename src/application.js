3e339c7ad01bea23fd88e9eafe7cc4292bed5f67
samover <samuel.overloop@gmail.com>
2015-11-12 16:39:43 +0000

    user can select location, openweather api implemented

--- src/application.js
+++ src/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  var owData;
+  var country;
+  var city;
+
   thermostat = new Thermostat();
   displayReset();
-  var owData;
   displayWeather();
 
   $('#up').click(function() {
@@ -29,20 +32,29 @@ $(document).ready(function() {
     displayReset();
   });
 
+  $('#fetchLocation').click(function() {
+    city = document.getElementById('city').value;
+    country = document.getElementById('country').value;
+    displayWeather(city, country);
+    event.preventDefault();
+  });
+
   function displayReset() {
     $('#showTemperature').text(thermostat.showTemperature());
     $('#display').attr('class', thermostat.display());
     $('span#isPowerSaving').text(thermostat.isPowerSaving() ? 'ON' : 'OFF');
   }
-
-  function displayWeather() {
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0', function(data) {
+  
+  function displayWeather(city, country) {
+      city = city || 'Rome';
+      country = country || 'US';
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=2de143494c0b295cca9337e1e96b00e0', function(data) {
       owData = data;
       $('#location').text(owData.name + ', ' + owData.sys.country);
       $('#temperature').text((owData.main.temp - 272.15).toFixed(1) + ' degrees Celsius');
       $('#windSpeed').text(owData.wind.speed + 'm/s');
       $('#weatherDescription').text(owData.weather.description);
-      $('$icon').text("<img src= " + owData.weather.icon + ">");
+      $('#icon').html("<img src='http://openweathermap.org/img/w/" + owData.weather[0].icon + ".png'>");
 
     });
   }
